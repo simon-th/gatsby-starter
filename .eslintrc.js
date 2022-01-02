6fb45b7712311aa4be6877feeaae00d221d81ed5
Simon Hoque <simon.thoque@gmail.com>
2022-01-02 13:47:47 -0600

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+  },
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  overrides: [
+    // Override some TypeScript rules just for .js files
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off", //
+      },
+    },
+  ],
+};
